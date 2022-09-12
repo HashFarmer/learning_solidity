7895da9d194e9477339b7f65327b3c94b512f40c
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-09-12 16:28:27 +0800

    Update CryptoKids.sol

--- CryptoKids.sol
+++ CryptoKids.sol
@@ -4,7 +4,41 @@ pragma solidity ^0.8.7;
 
 contract CryptoKids {
     // owner
-    address owner
+    address owner;
+    
+    constructor(){
+        owner = msg.sender; //msg.sender is the caller, as for contract ,it is the deployer.
+        
+    }
+    
+    //mapping is dictionary in python
+    //mapping(string => uint) public kids; // kids['string'] = uint; shouldn't it be mapping(address => uint) ?
+    
+    // does it consume gas?
+    struct Kid{
+        address walletAdress;
+        string firstName;
+        string lastName;
+        uint releaseTime;
+        uint amount;
+        bool canWithdraw;
+        
+    }
+    
+    Kid[] public kids;
+    
+    //function without return
+    //TypeError: Data Location must be "memory" or "calldata" for parameter in function , but none was given
+    function addKid(address walletAdress, string firstName, string lastName, uint releaseTime, uint amount, bool canWithdraw) public {
+        // one Kid object is complex, push one Kid into kids
+        kids.push(Kid(walletAdress, firstName, lastName, releaseTime, amount, canWithdraw));
+    
+    }
+    
+    
+    
+    
+    
 
 
 
