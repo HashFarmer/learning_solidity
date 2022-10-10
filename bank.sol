57c76e98cabc11897b882a9f2975b65fe998fde1
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-10-11 01:09:25 +0800

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -2,13 +2,41 @@
 pragma solidity ^0.8.13;
 
 contract Bank {
+
+    struct user {
+        uint id;
+        address account;
+        uint balance;
+        uint is_created;
+    }
+
+    modifier only_new_user(){
+        if( Balance[msg.sender].is_created == 1 ){
+            revert();
+        }
+        _;
+    }
+
+
+
     // 不需要是数组就可以存储多个账户
     // 类比与python中的dict类型
-    mapping(address => uint) public Balance;
+    // 但是mapping没有lenght属性！
+    mapping(address => user) public Balance;
 
-    function get_user_number() public returns(uint){
-        return Balance.;
+    uint user_counter = 0;
+
+    // 应该添加一个modifier，已经创建过账户的人不能再次创建账户
+    function create_user() public only_new_user{
+        Balance[msg.sender] = user(user_counter++, msg.sender, 0, 1);
+
     }
+
+    function get_user_number() public view returns(uint){
+        return user_counter;
+    }
+
+    /*
 
     function user_deposit() public payable {
         Balance[msg.sender] = msg.value;
@@ -22,4 +50,5 @@ contract Bank {
         // Reset the value to the default value.
         delete Balance[_addr];
     }
+    */
 }
